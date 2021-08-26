0bf9370c6633a1a0314815c26e4acecf5b6f8f3b
IamJagritiK <30434051+IamJagritiK@users.noreply.github.com>
2021-08-26 17:24:54 +0530

    Added logic for delimiters

--- src/main/java/com/incubyte/StringUtils.java
+++ src/main/java/com/incubyte/StringUtils.java
@@ -1,11 +1,14 @@
 package main.java.com.incubyte;
 
-public class StringUtils {
-    private StringUtils(){}
+import java.util.Arrays;
 
-    public static boolean isEmpty(String num){
-        if(num == null) {
-            return  true;
+public class StringUtils {
+    private StringUtils() {
+    }
+
+    public static boolean isEmpty(String num) {
+        if (num == null) {
+            return true;
         }
         return num.isEmpty();
     }
@@ -21,18 +24,37 @@ public class StringUtils {
     }
 
     private static int parInt(String input) {
-        try{
+        try {
             return Integer.parseInt(input);
         } catch (NumberFormatException ex) {
             return 0;
         }
     }
+
     private static ParsedInput parseInputString(String inp) {
-        if(!inp.startsWith("//")){
+        if (!inp.startsWith("//")) {
             return ParsedInput.of("[,\n]", inp);
         }
         String[] parts = inp.split("\n");
-        return ParsedInput.of( parts[0].substring(2), parts[1]);
+        String delimiters = getDelimiters(parts[0].substring(2));
+        return ParsedInput.of(delimiters, parts[1]);
+    }
 
+    private static String getDelimiters(String input) {
+        String[] delimiters = input.split("]");
+        return Arrays.stream(delimiters).map(str -> str.substring(1))
+                .map(StringUtils::sanitize)
+                .reduce("", (s1, s2) -> s1 + "|" + s2);
+    }
+
+    private static String sanitize(String str) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : str.toCharArray()) {
+            if (c == '*') {
+                sb.append("\\");
+            }
+            sb.append(c);
+        }
+        return sb.toString();
     }
 }
